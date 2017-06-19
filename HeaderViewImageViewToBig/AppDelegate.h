d7e5e5e7d8729ba3ae4ea20f5faf6c9e2691d668
UserLuoPan <799680236@qq.com>
2017-06-19 15:16:23 +0800

    Initial Commit

---
+++ HeaderViewImageViewToBig/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeaderViewImageViewToBig
+//
+//  Created by 咖达 on 2017/6/19.
+//  Copyright © 2017年 咖达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeaderViewImageViewToBig/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeaderViewImageViewToBig
+//
+//  Created by 咖达 on 2017/6/19.
+//  Copyright © 2017年 咖达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
